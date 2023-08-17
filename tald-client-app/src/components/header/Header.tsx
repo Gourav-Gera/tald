3ff760e5d076340864d2c23b6df1c26d276144e4
Gourav <gouravgera12345@gmail.com>
2023-08-17 13:15:53 +0530

    commit design changes

--- tald-client-app/src/components/header/Header.tsx
+++ tald-client-app/src/components/header/Header.tsx
@@ -1,103 +1,132 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import Avatar from "@mui/material/Avatar";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import Divider from "@mui/material/Divider";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Tooltip from "@mui/material/Tooltip";
-import styles from "./Header.module.scss"
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Image from 'next/image';
+import headerLogo from "../../assets/images/header-logo.png";
+import styles from "./Header.module.scss";
+import SearchIcon from '@mui/icons-material/Search';
 
-export default function Header() {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
+interface Props {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window?: () => Window;
+}
+
+const drawerWidth = 240;
+const navItems = ['Designers', 'Editorial', 'Contact'];
+
+export default function DrawerAppBar(props: Props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
   };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+
+  const drawer = (
+    <Box className={styles.headerSecWrapper} onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+			<Image
+				src={headerLogo}
+				width={84}
+				height={35}
+				className={styles.logoImage}
+				alt="logo image"
+			/>
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
   return (
-      <React.Fragment>
-          {/* headerWrap */}
-      <div className={styles.headerWrap}>
-        <Box
-          sx={{ display: "flex", alignItems: "center", textAlign: "center" }}
-        >
-          <Typography sx={{ minWidth: 100 }}>Contact</Typography>
-          <Typography sx={{ minWidth: 100 }}>Profile</Typography>
-          <Tooltip title="Account settings">
-            <IconButton
-              onClick={handleClick}
-              size="small"
-              sx={{ ml: 2 }}
-              aria-controls={open ? "account-menu" : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? "true" : undefined}
-            >
-              <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
-            </IconButton>
-          </Tooltip>
-        </Box>
-        <Menu
-          anchorEl={anchorEl}
-          id="account-menu"
-          open={open}
-          onClose={handleClose}
-          onClick={handleClose}
-          PaperProps={{
-            elevation: 0,
-            sx: {
-              overflow: "visible",
-              filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-              mt: 1.5,
-              "& .MuiAvatar-root": {
-                width: 32,
-                height: 32,
-                ml: -0.5,
-                mr: 1,
-              },
-              "&:before": {
-                content: '""',
-                display: "block",
-                position: "absolute",
-                top: 0,
-                right: 14,
-                width: 10,
-                height: 10,
-                bgcolor: "background.paper",
-                transform: "translateY(-50%) rotate(45deg)",
-                zIndex: 0,
-              },
-            },
+    <Box className={styles.headerSecWrapper} sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar component="nav">
+        	<Toolbar>
+				<IconButton
+					color="inherit"
+					aria-label="open drawer"
+					edge="start"
+					className={styles.iconColor}
+					onClick={handleDrawerToggle}
+					sx={{ mr: 2, display: { sm: 'none' } }}
+				>
+					<MenuIcon className='' />
+				</IconButton>
+				<Typography
+					variant="h6"
+					component="div"
+					className={styles.imgLogo}
+					sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+				>
+					<Image
+						src={headerLogo}
+						width={84}
+						  height={35}
+						  className={styles.logoImage}
+						alt="logo image"
+						
+					/>
+				</Typography>
+          	<Box className={styles.navMenu} sx={{ display: { xs: 'none', sm: 'block' } }}>
+					{navItems.map((item) => (
+					<Button key={item} sx={{ color: '#333' }}>
+						{item}
+					</Button>
+					))}
+				</Box>
+				<Box className="buttonRegister"  sx={{ ml: 'auto'  }}>
+				  	<IconButton size="large" aria-label="search" >
+            		<SearchIcon />
+					</IconButton>
+					<Button className='primaryGray' size="medium">Register</Button>
+				</Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
           }}
-          transformOrigin={{ horizontal: "right", vertical: "top" }}
-          anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
         >
-          <MenuItem onClick={handleClose}>
-            <Avatar /> Profile
-          </MenuItem>
-          <MenuItem onClick={handleClose}>
-            <Avatar /> My account
-          </MenuItem>
-          <Divider />
-          <MenuItem onClick={handleClose}>
-            <ListItemIcon></ListItemIcon>
-            Add another account
-          </MenuItem>
-          <MenuItem onClick={handleClose}>
-            <ListItemIcon></ListItemIcon>
-            Settings
-          </MenuItem>
-          <MenuItem onClick={handleClose}>
-            <ListItemIcon></ListItemIcon>
-            Logout
-          </MenuItem>
-        </Menu>
-          </div>
-          {/* header close */}
-    </React.Fragment>
+          {drawer}
+        </Drawer>
+      </Box>
+      
+    </Box>
   );
 }
+
